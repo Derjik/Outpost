eee986dff8482aff7a9e601ae0ae96473dd47289
Julien Derjik Laurent <julien.laurent@engineer.com>
2018-10-23 09:13:33 +0200

    Update main() and its comments

--- main.cpp
+++ main.cpp
@@ -21,38 +21,33 @@ using namespace std;
  *     L  C  R
  *     BL B BR
  * o Implement zoom
- * o Implement time
  * o Implement layers (shade and stack contexts when drawing)
  * o (Maybe) improve text displaying (handle newline ?)
  */
 
 int main(int argc, char ** argv)
 {
-	/* Initialize SDL library and additional modules */
 	SDL_Init(SDL_INIT_EVERYTHING);
 	IMG_Init(IMG_INIT_PNG);
 	TTF_Init();
 
-	/* Set DEBUG mode for all logs */
 	SDL_LogSetAllPriority(SDL_LOG_PRIORITY_DEBUG);
 
 	{
-		/* Perform introspection */
 		Introspection::perform();
 
-		/* Initialize TTF fonts */
 		std::shared_ptr<TrueTypeFontManager> ttfManager(
 			new TrueTypeFontManager({ "courier", "arial" }));
 
-		/* Instantiate window manager */
 		std::shared_ptr<WindowManager> windowManager(
 			new WindowManager());
 
-		/* Instantiate game controller manager */
 		std::shared_ptr<GameControllerManager> gameControllerManager(
 			new GameControllerManager());
 
-		/* Instantiate main window */
+		std::shared_ptr<Platform> platform(new Platform(
+			windowManager, gameControllerManager));
+
 		windowManager->add("mainWindow", "SDL Main Window",
 							SDL_WINDOWPOS_CENTERED,
 							SDL_WINDOWPOS_CENTERED,
@@ -61,9 +56,6 @@ int main(int argc, char ** argv)
 							SDL_WINDOW_SHOWN|SDL_WINDOW_RESIZABLE,
 							SDL_RENDERER_ACCELERATED,
 							ttfManager);
-
-		std::shared_ptr<Platform> platform(new Platform(
-			windowManager, gameControllerManager));
 
 		std::shared_ptr<Menu::Model> menuModel(new Menu::Model);
 		std::shared_ptr<GameContext> menu(new GameContext(
@@ -79,11 +71,9 @@ int main(int argc, char ** argv)
 		engine->run();
 	}
 
-	/* Close all modules */
 	IMG_Quit();
 	TTF_Quit();
 	SDL_Quit();
 
-	/* Finish */
 	return 0;
 }
