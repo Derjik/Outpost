0d7127b647fcca907caf7f0eec93d53a6a26887a
Julien "Derjik" Laurent <julien.laurent@engineer.com>
2020-11-04 00:23:45 +0100

    Add line feeds to demo text

--- Activities/Text.cpp
+++ Activities/Text.cpp
@@ -230,8 +230,8 @@ void Text::View::display(void)
 		"aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco "
 		"laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure "
 		"dolor in reprehenderit in voluptate velit esse cillum dolore eu "
-		"fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, "
-		"sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		"fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, "
+		"sunt in culpa qui officia deserunt mollit anim id est laborum.\n",
 		"courier", _model->getFontSize(), { 255, 255, 255, 255 },
 		_model->getDrawSpace());
 }
